1926dffb7715b14b3b41b7ba7c68d2cdbdba17af
Taylor Y <tdy721@gmail.com>
2023-02-08 12:52:18 -0700

    found feed stuff

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,7 +7,7 @@ import * as $0 from "./routes/_404.tsx"
 import * as $1 from "./routes/api/joke.ts"
 import * as $2 from "./routes/bird/[index].tsx"
 import * as $3 from "./routes/bird/index.tsx"
-import * as $4 from "./routes/feed.xml.ts"
+import * as $4 from "./routes/feed.ts"
 import * as $5 from "./routes/index.tsx"
 import * as $6 from "./routes/uploads/[file].js"
 import * as $7 from "./routes/week1.tsx"
@@ -19,7 +19,7 @@ const manifest = {
     "./routes/api/joke.ts": $1,
     "./routes/bird/[index].tsx": $2,
     "./routes/bird/index.tsx": $3,
-    "./routes/feed.xml.ts": $4,
+    "./routes/feed.ts": $4,
     "./routes/index.tsx": $5,
     "./routes/uploads/[file].js": $6,
     "./routes/week1.tsx": $7,
