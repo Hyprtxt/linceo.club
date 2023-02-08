524a451c984276fe3b1317b1a8cec9a9c353e93b
Taylor Y <tdy721@gmail.com>
2023-02-08 13:03:37 -0700

    tests

---
+++ test/1_puppet_test.js
@@ -0,0 +1,43 @@
+import { assertEquals } from "$std/testing/asserts.ts"
+import { freshPuppetTestWrapper } from "@/test/runner.js"
+import { BASE_URL } from "@/utils/config.js"
+
+Deno.test(
+  "Public Pages Testing",
+  freshPuppetTestWrapper(async (t, page) => {
+    await t.step("The homepage should work", async () => {
+      const response = await page.goto(`${BASE_URL}`, {
+        waitUntil: "networkidle2",
+      })
+      assertEquals(response.status(), 200)
+    })
+
+    await t.step("The Linceo Bird page should work", async () => {
+      const response = await page.goto(`${BASE_URL}/bird`, {
+        waitUntil: "networkidle2",
+      })
+      assertEquals(response.status(), 200)
+    })
+
+    await t.step("Pagination link 1 should work", async () => {
+      const response = await page.goto(`${BASE_URL}/bird?p=1`, {
+        waitUntil: "networkidle2",
+      })
+      assertEquals(response.status(), 200)
+    })
+
+    await t.step("Pagination link 2 should work", async () => {
+      const response = await page.goto(`${BASE_URL}/bird?p=2`, {
+        waitUntil: "domcontentloaded",
+      })
+      assertEquals(response.status(), 200)
+    })
+
+    await t.step("The error page should 404", async () => {
+      const response = await page.goto(`${BASE_URL}/404`, {
+        waitUntil: "networkidle2",
+      })
+      assertEquals(response.status(), 404)
+    })
+  }),
+)
