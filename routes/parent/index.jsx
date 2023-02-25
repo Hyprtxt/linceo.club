d3e63582c8fcdebadb31b573386af26ad5b85ec9
Taylor Y <tdy721@gmail.com>
2023-02-25 09:14:49 -0700

    upload and upgrade deno version

--- routes/parent/index.jsx
+++ routes/parent/index.jsx
@@ -1,12 +1,12 @@
 import { PageWrapper } from "@/routes/index.jsx"
 import { API_URL, TOKEN } from "@/utils/config.js"
 import { redirect } from "@/utils/mod.js"
-import { createContext } from "https://esm.sh/v106/preact@10.11.0/src/index"
+// import { createContext } from "https://esm.sh/v106/preact@10.11.0/src/index"
 import { tw } from "twind"
 
 export const handler = {
   GET: (_req, ctx) => {
-    console.log(ctx.state, "that")
+    // console.log(ctx.state, "that")
     if (!ctx.state.jwt) {
       return redirect("/unauthorized")
     }
@@ -17,6 +17,28 @@ export const handler = {
     // })
     //   .then(async (res) => await res.json())
     return ctx.render({ ...ctx.state })
+  },
+  POST: async (req, ctx) => {
+    const data = await req.formData()
+    const body = new FormData()
+    // console.log(data)
+    // console.log(data.get("files.media"))
+
+    body.set("files.media", data.get("media"))
+    body.set("data", JSON.stringify({ caption: data.get("caption") }, null, 0))
+
+    console.log(body)
+    const snap = await fetch(`${API_URL}/snaps?populate=*`, {
+      method: "POST",
+      body,
+      headers: new Headers({
+        Authorization: `Bearer ${ctx.state.jwt}`,
+      }),
+    })
+      .then(async (res) => await res.json())
+    console.log(snap, "snap")
+    return redirect("/parent")
+    // return ctx.render({ ...ctx.state })
   },
 }
 
@@ -33,7 +55,23 @@ export default function ParentPage({ data }) {
         <p class={tw`text-yellow mb-4`}>
           Welcome to the upload test page
         </p>
-        <input type="file" accept="image/*;capture=camera" />
+        <form method="POST">
+          <input
+            type="file"
+            name="media"
+            accept="image/*;capture=camera"
+          />
+          <textarea
+            rows={3}
+            name="caption"
+            class="bg-black border-1 border-white rounded my-2 px-2 w-full"
+          />
+          <input
+            type="submit"
+            value="upload"
+            class="bg-black border-1 border-white rounded px-2 cursor-pointer hover:bg-yellow hover:text-black active:bg-green active:text-black"
+          />
+        </form>
         {
           /* <p class="text-red mt-2">
           That's all for now.
